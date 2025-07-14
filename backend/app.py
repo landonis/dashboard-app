c9e93edeb63fd2054f888b24c60e6339f472da33
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2025-07-14 11:30:06 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -52,6 +52,9 @@ class User(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        print("Input password:", password)
+        print("Stored hash:", self.password_hash)
+        print("Password check:", check_password_hash(self.password_hash, password))
         return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
