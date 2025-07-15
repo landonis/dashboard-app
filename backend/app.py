ee453544102e0bc3b486587b77caae72c72b94ef
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2025-07-15 07:54:14 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -39,7 +39,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///das
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize extensions
-db = SQLAlchemy()
 jwt = JWTManager()
 db.init_app(app)
 jwt.init_app(app)
